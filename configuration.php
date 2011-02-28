0fbb840a3e1e0a6fca9f668448b13470b29741c8
Simon Sablowski <contact@simsab.net>
2011-02-28 09:46:43 +0000

    initial commit

---
+++ configuration.php
@@ -0,0 +1,34 @@
+<?php
+
+$configuration = array();
+
+$configuration['pathApplication'] = dirname(__FILE__) . '/';
+
+$configuration['basePath'] = '/';
+
+$configuration['includeDirectories'] = array(
+	$configuration['pathApplication'],
+	$configuration['pathApplication'] . '../nacho/'
+);
+
+$configuration['Localization'] = array(
+	'default' => 'en',
+	'en' => array(
+		'language' => 'en',
+		'locale' => 'en_GB',
+		'name' => 'English'
+	),
+	'de' => array(
+		'language' => 'de',
+		'locale' => 'de_DE',
+		'name' => 'Deutsch'
+	)
+);
+
+$configuration['Request'] = array(
+	'defaultQuery' => 'Static/show/index',
+	'aliasQueries' => array()
+);
+
+// $configuration['debugMode'] = TRUE;
+$configuration['debugMode'] = FALSE;
