059f935887e424716a7407d36ee334a4aaf18347
simsab@simsab.de <simsab@simsab.de@6d42f104-5abb-adae-5f0a-09b3412f9308>
2011-02-28 09:46:43 +0000

    initial commit

---
+++ trunk/views/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->localize('en'); ?>">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta http-equiv="Content-Language" content="<?php echo $this->localize('en'); ?>"/>
+		<title><?php echo $this->localize('smartassumption'); ?></title>
+		<link rel="canonical" href="<?php echo $this->localize('http://www.smartassumption.com/en'); ?>"/>
+		<meta name="dc.title" content="<?php echo $this->localize('smartassumption'); ?>"/>
+		<meta name="revisit-after" content="1 day"/>
+		<link href="<?php echo $this->getConfiguration('basePath'); ?>css/screen.css" rel="stylesheet" title="Default" type="text/css" media="screen"/>
+		<link type="image/x-icon" href="<?php echo $this->getConfiguration('basePath'); ?>img/favicon.ico" rel="shortcut icon"/>
+	</head>
+	<body>
+		<div id="document">
+			<div id="head">
+				<h1 id="logo">
+					<a href="<?php echo $this->localize('/en'); ?>" title="<?php echo $this->localize('smartassumption'); ?>"><?php echo $this->localize('<em>smartass</em>umption'); ?></a>
+				</h1>
+			</div>
+			<div id="body">
+				<p>
+					<?php echo $this->localize('Under construction.'); ?>
+
+				</p>
+			</div>
+		</div>
+		<div id="foot">
+			<p id="copyright">
+				<?php echo $this->localize('&copy; <a href="/en" title="smartassumption">smartassumption</a> 2011'); ?>
+
+			</p>
+		</div>
+		<script type="text/javascript">
+		<!--
+		var gaJsHost = 'https:' == document.location.protocol ? 'https://ssl.' : 'http://www.';
+		document.write(unescape('%3Cscript src="' + gaJsHost + 'google-analytics.com/ga.js" type="text/javascript"%3E%3C/script%3E'));
+		try {
+			var pageTracker = _gat._getTracker('UA-16346904-1');
+			pageTracker._initData();
+			pageTracker._trackPageview();
+		} catch (error) {
+			
+		}
+		//-->
+		</script>
+	</body>
+</html>
